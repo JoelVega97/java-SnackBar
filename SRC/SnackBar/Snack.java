60a89230bac29cd74d4bf15b72795c1061474410
joel vega <joelvega97@yahoo.com>
2020-09-28 18:43:36 -0400

    Filled out Snack.java

--- SRC/SnackBar/Snack.java
+++ SRC/SnackBar/Snack.java
@@ -2,8 +2,57 @@ package SnackBar;
 
 public class Snack {
     //fields (knows)
-    private static int MaxId = 0;
+    private static int maxId = 0;
 
     private int id;
     private String name;
+    private int quantity;
+    private double cost;
+    private int vmID;
+
+    public Snack(String name, int quantity, double cost, int vmID){
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+        this.quantity = quantity;
+        this.cost = cost;
+        this.vmID = vmID;
+    }
+    //sets & gets
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public double getCost(){
+        return cost;
+    }
+    public void setCost(double cost){
+        this.cost = cost;
+    }
+    
+    public int getVmID(){
+        return vmID;
+    }
+    public void setVmID(int vmID){
+        this.vmID = vmID;
+    }
 }
