fda44dea88d985c236f630ed504318ceb177d892
joel vega <joelvega97@yahoo.com>
2020-09-28 18:32:37 -0400

    made the base files.

---
+++ SRC/SnackBar/Snack.java
@@ -0,0 +1,9 @@
+package SnackBar;
+
+public class Snack {
+    //fields (knows)
+    private static int MaxId = 0;
+
+    private int id;
+    private String name;
+}
---
+++ SRC/SnackBar/VendingMachine.java
@@ -0,0 +1,5 @@
+package SnackBar;
+
+public class VendingMachine {
+    
+}
